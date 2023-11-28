39dbbd817ba0596aded2869bfe9fddc5be21fd1d
reebxu459 <rebbi.xu123@gmail.com>
2023-11-28 00:40:12 -0500

    finished setup in harness

--- main.cc
+++ main.cc
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <fstream>
 #include "game.h"
 // #include "player.h"
 
@@ -22,62 +23,71 @@ int main() {
     Player p2;
     cout << "Welcome to RAIInet!" << endl;
     // setup
-    string line;
     cout << "starting setup." << endl;
-
-    // -ability1 <order>
-    cout << "specify abilities for Player One using command: -ability1 <order>" << endl;
-    cout << "Abilities:" << "\n" << "- L: Link Boost" << "\n" << "- F: Firewall"
-    << "\n" << "- D: Download" << "\n" << "- P: Polarize" << "\n" << "- S: Scan"
-    << "\n" << "- O: Move Link" << "\n" << "- A: Sabotage" << "\n" << "- T: Strength Boost" <<endl;
-    cout << "example: -ability1 LFDSP" << "\n" << endl;
     
-    string command;
-    getline(cin, line);
-    stringstream ss{line};
-    while (ss >> command && command != "-ability1") {
-        cout << "invalid command. Try again!" << "\n" << endl;
-    }
-
-    // adding the abilities
-    char ability;
-    bool needDefault = true; // check if the player did not specify abilities
-    while (ss >> ability) {
-        needDefault = false; // set to false
-        p1.addAbility(ability);
-    }
-    // check if need default abilities
-    if (needDefault == true) {
-        p1.addAbility('L');
-        p1.addAbility('F');
-        p1.addAbility('D');
-        p1.addAbility('S');
-        p1.addAbility('P');
-    }
-
-    // TODO: just finished 
-
-    
-
-    
-
-    while (cin >> command) {
+    string line;
+    while (getline(cin, line)) {
+        stringstream ss{line};
+        string command;
+        ss >> command;
+        
         if (command == "-ability1") {
-            /* 
-            Abilities:
-            - L: Link Boost
-            - F: Firewall
-            - D: Download
-            - P: Polarize
-            - S: Scan
-            - O: Move Link
-            - A: Sabotage
-            - T: Strength Boost
-            */
-            
+            char ability;
+            bool needDefault = true; // remains true if the player did not specify abilities
+            while (ss >> ability) {
+                needDefault = false; // set to false
+                p1.addAbility(ability);
+            }
+            // if need default abilities
+            if (needDefault == true) {
+                p1.addAbility('L');
+                p1.addAbility('F');
+                p1.addAbility('D');
+                p1.addAbility('S');
+                p1.addAbility('P');
+            }
         }
-    }
 
+        if (command == "-ability2") {
+            char ability;
+            bool needDefault = true; // remains true if the player did not specify abilities
+            while (ss >> ability) {
+                needDefault = false; // set to false
+                p2.addAbility(ability); // ability type is string
+            }
+            // if need default abilities
+            if (needDefault == true) {
+                p2.addAbility('L');
+                p2.addAbility('F');
+                p2.addAbility('D');
+                p2.addAbility('S');
+                p2.addAbility('P');
+            }
+        }
+
+        if (command == "-link1") {
+            string fileName;
+            char ids ['a','b','c','d','e','f','g'];
+            if (ss >> fileName) {
+                fstream f {fileName};
+                for (int i = 0; i < 8; ++i) {
+                    string link;
+                    f >> link;
+                    p1.addLink(ids[i], link);
+                }
+            }
+            else {
+                // TODO: randomize module
+            }     
+        }
+
+        // TODO: -link2
+
+        if (command == "graphics") {
+            cout << g << endl; // TODO: unsure
+        }
+
+    }
 }
 
 
