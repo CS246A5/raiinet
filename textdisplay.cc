9b6a8e138348ac26cc418faa52257c7b72fda105
reebxu459 <rebbi.xu123@gmail.com>
2023-12-02 03:54:46 -0500

    finished TextDisplay implementation

---
+++ textdisplay.cc
@@ -0,0 +1,29 @@
+#include "textdisplay.h"
+#include "cell.h"
+
+using namespace std;
+
+TextDisplay::TextDisplay() {
+    for (int i = 0; i < 8; ++i) {
+        for (int j = 0; j < 8; ++j) {
+            theDisplay[i][j] = '.';
+        }
+    }
+}
+
+TextDisplay::~TextDisplay() {}
+
+void TextDisplay::notify(Cell &c) {
+    theDisplay[c.getRow()][c.getColumn()] = c.getState();
+}
+
+std::ostream &operator<<(std::ostream &out, const TextDisplay &td) {
+    for (int i = 0; i < 8; ++i) {
+        for (int j = 0; j < 8; ++j) {
+            out << td.theDisplay[i][j];
+        }
+        out << endl;
+    }
+    return out;
+}
+
